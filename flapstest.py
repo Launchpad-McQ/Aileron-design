08bf3288b4124e9ef269e5e7e935056e86395295
Launchpad-McQ <magnus.k.wiberg@gmail.com>
2017-07-07 21:04:44 +0200

    fixed script

--- flapstest.py
+++ flapstest.py
@@ -10,14 +10,15 @@ import numpy as np
 
 # Input of flap deflection angles.
 delta1 = float(raw_input("Lower delta limit:") or 14)
-delta2 = float(raw_input("Upper delta limit:") or 14.4)
+delta2 = float(raw_input("Upper delta limit:") or 15)
 dstep = float(raw_input("delta step length:") or 0.2)
-airfoil = str(raw_input("Enter airfoil file name: ") or "load naca632615.dat")
-alfa = str(raw_input("Alfa (angle of attack of wing. default 2):") or 2)
+airfoil = str(raw_input("Enter airfoil file name: ") or "naca632615.dat")
+alfa = str(raw_input("Alfa (angle of attack of wing. default 2):") or 0)
 flaphingexpos =  str(raw_input("Flap hinge x position: ") or 0.75)
-bolcal = str(raw_input("Calculate roll helix angle? (y/n)") or "n")
+bolcal = str(raw_input("Calculate roll helix angle? (y/n)") or "y")
 
 outfile = "flaptest" + str(delta1) + "-"+ str(delta2) + "__" + str(time.strftime("%H_%M_%S")) + ".txt" 
+zeroaoaoutfile = "zeroAOA"+ str(time.strftime("%H_%M_%S")) + ".txt"
 
 # Starting sumprocess
 ps = sp.Popen(["xfoil.app/Contents/Resources/xfoil"],stdin=sp.PIPE,stdout=None,stderr=None)
@@ -48,14 +49,15 @@ def setflap(delta):
 
 # Loading airfoil.
 def load_smooth(airfoil):
-	issueCmd(airfoil) # Load the dat file.
+	issueCmd("load " + airfoil) # Load the dat file.
 	issueCmd("MDES") # Go to the MDES menu.
 	issueCmd("FILT") # Smooth any variations in the dat file data.
 	issueCmd("EXEC") # Execute the smoothing.
 	issueCmd("") # Back to main menu.
+	issueCmd("PANE")
 
 # Simulating airfoil for selected deltas.
-def deltasim(airfoil, outfile, delta1, delta2, dstep):
+def deltasim(airfoil, outfile, delta1, delta2, dstep, alfa):
 	load_smooth(airfoil)
 	issueCmd("OPER") # Go to the OPER menu.
 	issueCmd("ITER 1000") # Max number of iterations set to 70 for convergence.
@@ -72,11 +74,38 @@ def deltasim(airfoil, outfile, delta1, delta2, dstep):
 		setflap(str(delta))
 		issueCmd("OPER")
 		issueCmd("alfa" + alfa)
-		#time.sleep(1)
 		issueCmd("")
 	issueCmd("QUIT")
 
+def alpha0sim(airfoil,zeroaoaoutfile):
+	load_smooth(airfoil)
+	issueCmd("OPER") # Go to the OPER menu.
+	issueCmd("ITER 1000") # Max number of iterations set to 70 for convergence.
+	issueCmd("RE 100000") # Set Reynolds number.
+	issueCmd("VISC 100000") # Set viscous calculation with Reynolds number.
 
+	issueCmd("PACC") # Enter PACC menu.
+	issueCmd(zeroaoaoutfile) # Output file for xfoil polars.
+	issueCmd("") # No buffer file.
+	issueCmd("") #  Back to main menu.
+
+
+	issueCmd("OPER")
+	issueCmd("alfa 0")
+	issueCmd("")
+	issueCmd("PACC")
+	issueCmd("VISC")
+	issueCmd("QUIT")
+
+
+
+
+# Running simulation.
+alpha0sim(airfoil,zeroaoaoutfile)
+ps = sp.Popen(["xfoil.app/Contents/Resources/xfoil"],stdin=sp.PIPE,stdout=None,stderr=None)
+
+deltasim(airfoil, outfile, delta1, delta2, dstep, alfa)
+ps.wait()
 
 #__________________________________________________________________________________________
 # Plotting C_l vs delta
@@ -120,12 +149,12 @@ def getClarray(outfile):
 
 	# Extracting Cl.
 	array = [row[1] for row in data_array]
-	return array
-
+	cd0 = [row[2] for row in data_array]
+	return [array, cd0]
 
 # Plot Cl vs delta. Might miss Cl for some deltas since not always converging. This is ignored and thus the delta axis is not entierly true.
 def plotClvd(outfile, delta1, delta2, dstep):
-	clarray = getClarray(outfile)
+	clarray = getClarray(outfile)[0]
 	length = len(clarray)
 	clarray = np.array(clarray)
 
@@ -146,14 +175,13 @@ def plotClvd(outfile, delta1, delta2, dstep):
 
 	fit = np.polyfit(x,y,1)
 	cldadeg = fit[0]
-	print "clda (per deg): " + str(cldadeg)
 	fit_fn = np.poly1d(fit) 
 
 	if noncon == 0:
 		print "clda (/ deg): " + str(cldadeg)
 	else:
 		print "Warning!!! Missing values of Cl since not converged"
-		print "bad clda (per deg): " + str(cldadeg)
+		print "bad clda (/ deg): " + str(cldadeg)
 
 	plt.plot(x,y, 'ro', x, fit_fn(x), '--k')
 	
@@ -163,9 +191,7 @@ def plotClvd(outfile, delta1, delta2, dstep):
 
 
 
-# Running simulation.
-deltasim(airfoil, outfile, delta1, delta2, dstep)
-ps.wait()
+
 
 # Plotting Cl vs delta.
 cldadeg = plotClvd(outfile, delta1, delta2, dstep)
@@ -173,25 +199,46 @@ cldadeg = plotClvd(outfile, delta1, delta2, dstep)
 
 #_________________________________________________________________________________
 # Calculation.
-if (bolcal == "y"):
+
+def setgeo():
 
 	b = float(raw_input("Wing span: ") or 1.7)
 	S = float(raw_input("Wing area: ") or 0.358)
 	taper = float(raw_input("Wing taper: ") or 0.45)
 	cord = float(raw_input("Wing cord: ") or 0.122)
-	delta = float(raw_input("max aileron deflection (degrees): ") or delta2)
+	delta = float(raw_input("Max aileron deflection (degrees): ") or delta2)
+	b1 = float(raw_input("Inner position: ") or 0.38)
+	b2 = float(raw_input("Outer positien: ") or 0.8)
 
 	deltarad = 2*np.pi*delta/360
 	p = 2*(1-taper)
 
-	Clp = -0.358
+	return [b,S,taper,cord,delta,deltarad,p,b1,b2]
+
+if (bolcal == "y"):
+
+	[b,S,taper,cord,delta,deltarad,p,b1,b2] = setgeo()
+
+	cda = 2*np.pi
+	cd0 = getClarray(zeroaoaoutfile)[1][0]
+	print "cd0:" + str(cd0)
+	Clp = -4*(cda+cd0)/(S*b**2)*cord*((b**3)/(3*8)-(p/b)*(b**4)/(4*16))
+	print "Clp:" + str(Clp)
+	#Clp = -0.358
 	cldarad = cldadeg*360/(2*np.pi)
 	print "clda (/rad)" + str(cldarad)
+
+	first = True
 	while True:
-		delta = float(raw_input("max aileron deflection (degrees): ") or delta)
-		print "Set position of aileron (ctrl c to exit)"
-		b1 = float(raw_input("Inner position: ") or 0.38)
-		b2 = float(raw_input("Outer positien: ") or 0.8)
+		if first:
+			first = False
+		else:
+			print "Try with new placement: (ctrl c to exit)"
+			delta = float(raw_input("Max aileron deflection (degrees) (enter to keep unchanged (delta = " + str(delta) + "))") or delta)
+			deltarad = 2*np.pi*delta/360
+			print "Set position of aileron"
+			b1 = float(raw_input("Inner position (enter to keep unchanged (b1 = " + str(b1) + ")): ") or b1)
+			b2 = float(raw_input("Outer position (enter to keep unchanged (b2 = " + str(b2) + ")): ") or b2)
 
 		Clda = (2*cldarad*cord/(S*b))*(((b2**2)/2-((p/b)*b2**3)/3)-((b1**2)/2-(p/b)*(b1**3)/3))
 		rollhelixangle = -deltarad*Clda/Clp
@@ -201,4 +248,5 @@ if (bolcal == "y"):
 		print str(rollhelixangle)
 		print "(Fighter 0.09, cargo/heavy transport 0.07"
 		print "\n"
+
 
